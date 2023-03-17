c5814891d5f8b15ca303537a71bf2eaea07b4f01
DarioCabas <hz-hertzio@hotmail.com>
2023-03-17 16:10:10 -0500

    modify routes and load a image

--- projectml/app/main.py
+++ projectml/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from projectml.app.routers import products
-from projectml.app.routers import users
+from app.routers import products
+from app.routers import users
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
@@ -8,10 +9,10 @@ app = FastAPI()
 #Routers
 app.include_router(products.router)
 app.include_router(users.router)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
- 
 
